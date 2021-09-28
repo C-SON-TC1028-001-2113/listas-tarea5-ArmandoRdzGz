28bb22f15f53dff8daa8d056ad877f31915bef5e
ArmandoRdzGz <A01254249@itesm.mx>
2021-09-28 05:50:31 +0000

    sinduplicados_armandot5

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,8 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
 
+    num = int(input())
+    lista = []
+    i = 0
+
+    if num>0 : 
+        while i<num:
+            valor = input()
+            lista.append(valor)
+            i = i+1
+        print(lista)
+        sin_repetir = []
+        for i in lista : 
+            if i not in sin_repetir:
+                sin_repetir.append(i)
+        print(sin_repetir)
+    else: 
+        print('Error')
 
     pass
-
 if __name__=='__main__':
     main()
